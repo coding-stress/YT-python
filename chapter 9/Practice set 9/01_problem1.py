3031d2de39bf4c707b6b01dd2faa201695e2afb4
Abhishek Singh <abhishekabhi7433@gmail.com>
2026-05-20 18:35:04 +0530

    this is a program to find the word twinkle in the poem

---
+++ chapter 9/Practice set 9/01_problem1.py
@@ -0,0 +1,11 @@
+# Write a program to read the text from a given file 'poem.txt' and find out whether it contains the word 'teinkle'.
+
+f = open("poem.txt")
+
+poem = f.read()
+if("twinkle" in poem):
+    print("the word twinkle is present in the poem ")
+
+else:
+    print(" The word Twinklw is not present in the poem ")
+f.close
