bbd057f26236f6d3754dbccdc65bd8f856858bc4
Abhishek Singh <abhishekabhi7433@gmail.com>
2026-06-01 11:49:23 +0530

    solving problem 3 chapter 9

--- chapter 9/Practice set 9/03_problem3.py
+++ chapter 9/Practice set 9/03_problem3.py
@@ -1,2 +1,16 @@
 # Write a program to generate multiplaction table from 2 to 20 and write it to the different files. place this files in a folder for a 13 - year old.
 
+
+def generatetable(n):
+    table = "" #this variable is used to store all values in it 
+    for i in range (1 , 11):
+        table += f"{n} X {i} = {n*i}\n"
+
+    with open(f"Tables/table_{n}.txt", "w") as f: # use .txt to make it writable formate using Table/table_{n} for generate new file to store new table
+        f.write(table)
+
+
+for i in range (2 , 21):
+    generatetable(i)
+
+print("table is printed.")
