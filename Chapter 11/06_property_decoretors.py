8336a6210298ec57098c74084dcc242285891eb9
Abhishek Singh <abhishekabhi7433@gmail.com>
2026-06-19 16:46:26 +0530

    Solving problem 6 property decorator

--- Chapter 11/06_property_decoretors.py
+++ Chapter 11/06_property_decoretors.py
@@ -5,11 +5,20 @@ class Employee:
     def show(cls):
         print(f"The class attribute is {cls.a}")
 
+    @property 
+    def name (self):
+        return f"{self.fname} {self.lname}"
+    
+    @name.setter
+    def name (self , value):
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
+
 
 e = Employee()
 e.a = 45
-e.name = "coding_stress"
+e.name = "coding stress"
 
-print(e.name)
+print(e.fname)
 
 e.show()
