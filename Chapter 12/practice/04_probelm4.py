8cd398871b8fa733f9cbe9ca929baaa1a25752a1
Abhishek Singh <abhishekabhi7433@gmail.com>
2026-06-30 12:31:17 +0530

    making some correction of problem 4

--- Chapter 12/practice/04_probelm4.py
+++ Chapter 12/practice/04_probelm4.py
@@ -1,10 +1,9 @@
 # Write a program to display a/b where a and b are integers. If b = 0, display infinite by handling the 'ZeroDivisionError'.
 
-a= 5
-b= 0
+try:
+    a = int(input("Enter a :"))
+    b = int(input("Enter b :"))
+    print(a/b)
 
-if(b == 0):
-    raise ZeroDivisionError("Hey our program is not meant to divide numbes by zero")
-else:
-    print(f"The division a/b is {a/b}")
-
+except ZeroDivisionError as v:
+    print("Infinite")
