dfa70f17a37395b549e2f41d49ea5c302f565e7e
Abhishek Singh <abhishekabhi7433@gmail.com>
2026-06-30 12:38:02 +0530

    changing the file type from 'w' to 'a' to store all tables in table.txt

--- Chapter 12/practice/05_problem5.py
+++ Chapter 12/practice/05_problem5.py
@@ -4,6 +4,6 @@ Table = [ f"{num} X {i} = {num*i}" for i in range (1 , 11) ]
 
 # print(*Table,sep="\n")
 
-with open("Table.txt", "w") as f:
+with open("Table.txt", "a") as f:
     # f.write(str(Table))
     print(*Table,sep="\n",file=f) # This line helps to print new line after one loop is completed.
